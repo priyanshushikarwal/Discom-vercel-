59557af71b9c741eaa411932a4d81153f9c07bee
priyanshu <priyanshushikarwal@gmail.com>
2026-06-20 03:44:57 +0530

    B2 configuration and making the chnages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"78fc3012e45889657f72359b005af7beac47ba
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "575402450"
+    serviceWorkerVersion: "2262732851"
   }
 });
